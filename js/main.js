0b69d4fda312f5872eee705221a0ead9a86d9e28
Hanna Stoichyk <hannastoichyk@gmail.com>
2023-11-14 18:06:16 +0100

    commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
-// PROGRAMMA PRINCIPALE (Eseguito immediatamente) //
+/* PROGRAMMA PRINCIPALE (Eseguito immediatamente) 
 alert("Benvenuto a bordo!");
-alert("Completa tutti i campi richiesti");
+alert("Completa tutti i campi richiesti"); */
+
+//...///
+function userHasfinishDateCompletion(event) {
+  event.preventDefault();
+  console.log("Form funziona");
+}
